9cf34bff09f301f51bb35dadc514a448d5ce3b57
Deepesh Yadav <deepesh.yadav3@indiamart.com>
2026-07-26 14:07:08 +0530

    Lower RemoteAuth backupSyncIntervalMs to 10s for fast MongoDB session uploads

--- src/index.js
+++ src/index.js
@@ -264,7 +264,7 @@ async function startBot() {
       clientId: 'wa-link-deleter-session',
       dataPath: './',
       store: store,
-      backupSyncIntervalMs: 60000
+      backupSyncIntervalMs: 10000
     });
   } else {
     logger.info('Using LocalAuth session storage (.wwebjs_auth)...');
